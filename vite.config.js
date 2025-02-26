0926f3ac4d2593aa21d94c2ed4e19f50e7c03a4b
raineponce <ponceraine23@gmail.com>
2025-02-25 21:41:04 -0500

    added slash

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/updated-exercise-app-1',
+  base: '/updated-exercise-app-1/',
 })
